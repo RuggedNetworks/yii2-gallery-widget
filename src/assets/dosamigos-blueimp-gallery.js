4ac6a386490a975d47527fe000478c3b3a6d64cf
Alan <me@alanhollis.com>
2015-11-26 13:20:19 +0000

    Fix options not being passed to blueimp-gallery

--- src/assets/dosamigos-blueimp-gallery.js
+++ src/assets/dosamigos-blueimp-gallery.js
@@ -9,9 +9,10 @@ if (typeof dosamigos == "undefined" || !dosamigos) {
 dosamigos.gallery = (function($){
     var pub = {
         registerLightBoxHandlers: function(selector, options) {
+            var nestedOptions = options;
             $(document).off('click.gallery', selector).on('click.gallery', selector, function() {
                 var links = $(this).parent().find('a.gallery-item');
-                var options = options || {};
+                var options = nestedOptions || {};
                 options.index = $(this)[0];
                 blueimp.Gallery(links, options);
                 return false;
